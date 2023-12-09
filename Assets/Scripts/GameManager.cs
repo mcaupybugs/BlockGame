afa8c9c9f2cad6df3312644044dc6d01adcaf90d
Vishal Yadav <vishalydv.me@gmail.com>
2023-12-09 22:10:11 +0530

    adding game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject block;
+    public float maxX;
+    public Transform spawnPoint;
+    public float spawnRate;
+    public GameObject tapText;
+    public TextMeshProUGUI scoreText;
+
+    bool gameStarted = false;
+    int score = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)&&!gameStarted){
+            StartSpawning();
+            gameStarted=true;
+            tapText.SetActive(false);
+        }
+    }
+
+    void StartSpawning(){
+        InvokeRepeating("SpawnBlock", 0.5f, spawnRate);
+    }
+
+    void SpawnBlock(){
+        Vector3 spawnPos = spawnPoint.position;
+        spawnPos.x = Random.Range(-maxX, maxX);
+        Instantiate(block, spawnPos, Quaternion.identity);
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
